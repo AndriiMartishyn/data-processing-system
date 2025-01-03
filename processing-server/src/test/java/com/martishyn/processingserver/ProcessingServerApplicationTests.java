f9cb58713b18d355cef25aeceb0e87f29f09420f
AndriiMartishyn <contactgently@gmail.com>
2025-01-03 12:37:58 +0200

    Created mock-server and processing-server modules and initial structure

---
+++ processing-server/src/test/java/com/martishyn/processingserver/ProcessingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martishyn.processingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
